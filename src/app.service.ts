167a13dd9978c172760c98b1508a3bda45e075db
Anoop-LogCat <anoopbenzier1997@outlook.com>
2021-11-14 14:40:05 +0530

    install csv-parser & fs, get all books & magazines

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { CsvParser, ParsedData } from 'nest-csv-parser';
+import * as fs from 'fs';
+import { join } from 'path';
+import { Books } from './schema/book.schema';
+import { Magazines } from './schema/magazines.schema';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly csvParser: CsvParser) {}
+
+  async getBooks(): Promise<ParsedData<Books[]>> {
+    const readStream = fs.createReadStream(
+      join(__dirname, '..', 'src/data/books.csv'),
+    );
+    return await this.csvParser.parse(readStream, Books);
+  }
+
+  async getMagazines(): Promise<ParsedData<Magazines[]>> {
+    const readStream = fs.createReadStream(
+      join(__dirname, '..', 'src/data/magazines.csv'),
+    );
+    return await this.csvParser.parse(readStream, Magazines);
   }
 }
